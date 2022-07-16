913e82e7db28596a410573643ed4b63ba5acd177
aleksey-prokopchuk <prokop4huk@ukr.net>
2022-07-16 18:09:40 +0300

    Доробив завдання

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,59 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector('.gallery')
+
+
+//Рендер галереї по масиву данних за допомогою map
+// const imageLi = galleryItems.map(item => `<div class="gallery__item">
+//   <a class="gallery__link" href="${item.original}">
+//     <img
+//       class="gallery__image"
+//       src="${item.preview}"
+//       data-source="${item.original}"
+//       alt="${item.description}"
+//     />
+//   </a>
+//   </div>`
+
+// ).join('');
+
+//Рендер галереї по масиву данних за допомогою reduce
+const imageLi = galleryItems.reduce((acc, item) => acc + `<div class="gallery__item">
+<a class="gallery__link" href="${item.original}">
+<img
+class="gallery__image"
+src="${item.preview}"
+data-source="${item.original}"
+alt="${item.description}"
+/>
+</a>
+</div>`, '');
+
+//Добавляємо в список через innerHTML
+// gallery.innerHTML = imageLi;
+
+//Добавляємо  в список через insertAdjacentHTML
+gallery.insertAdjacentHTML('beforeend', imageLi);
+
+//   console.log(imageLi);
+
+
+// Реалізація делегування на div.gallery
+
+const handleClick = (event) => {
+    event.preventDefault()
+    if (!event.target.classList.contains('gallery__image')) {
+        return
+    }
+    console.log(event.target.dataset.source);
+    console.log('Бідбувся клік по зображенню' );
+    
+    //Отримання url великого зображення та відкриття повного зображення.
+    const instance = basicLightbox.create(`
+    <img src='${event.target.dataset.source}'>
+`)
+    instance.show()
+}
+
+gallery.addEventListener('click', handleClick);
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,18 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector('.gallery');
+console.log(gallery);
+
+//Створюю розмітку по масиву
+const imageLi = galleryItems.reduce((acc, item) => acc + `<a class="gallery__item" href="${item.original}">
+  <img class="gallery__image" src="${item.preview}" alt="${item.description}" title="${item.description}" />
+</a>`, '');
+console.log(imageLi);
+
+// Добавляю розмітку в DOM
+gallery.insertAdjacentHTML('beforeend', imageLi);
+
+
+    //Відкриття повного зображення з підписом під зображенням.
+var lightbox = new SimpleLightbox('.gallery a', { captionDelay:250 });
